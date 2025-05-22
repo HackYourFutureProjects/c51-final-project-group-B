f19464e9c5b24a0ec2f9ab2565163ca2abdc9017
lidyaT21 <156967948+lidyaT21@users.noreply.github.com>
2025-05-22 23:02:37 +0200

    Created NavBar Component

---
+++ client/src/components/NavBar/Nav.jsx
@@ -0,0 +1,96 @@
+import { Link } from "react-router-dom";
+import { useUser } from "../../util/UserContext";
+import "./NavBar.css";
+
+const Nav = () => {
+  const { user, logout } = useUser();
+  const isAuthorized = user !== null;
+
+  return (
+    <nav>
+      <div className="nav__header">
+        <div className="nav__logo">
+          <h2>OpportunityHunt</h2>
+        </div>
+      </div>
+
+      <ul className="nav__links">
+        {!isAuthorized && (
+          <>
+            <li>
+              <Link to="/jobs">Jobs</Link>
+            </li>
+            <li>
+              <Link to="/companies">Companies</Link>
+            </li>
+            <li>
+              <Link to="/feeds">Feeds</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact Us</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup" className="btn">
+                SignUp
+              </Link>
+            </li>
+          </>
+        )}
+
+        {isAuthorized && user?.role === "JobSeeker" && (
+          <>
+            <li>
+              <Link to="/jobs">Jobs</Link>
+            </li>
+            <li>
+              <Link to="/companies">Companies</Link>
+            </li>
+            <li>
+              <Link to="/feeds">Feeds</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact Us</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+            <li>
+              <Link to="/notifications">Notifications</Link>
+            </li>
+            <li>
+              <button onClick={logout}>Logout</button>
+            </li>
+          </>
+        )}
+
+        {isAuthorized && user?.role === "Employer" && (
+          <>
+            <li>
+              <Link to="/jobposts">Job Posts</Link>
+            </li>
+            <li>
+              <Link to="/feeds">Feeds</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact Us</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+            <li>
+              <Link to="/notifications">Notifications</Link>
+            </li>
+            <li>
+              <button onClick={logout}>Logout</button>
+            </li>
+          </>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;

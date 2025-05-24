f13e34409d0d505e468809967a202dc30ff525aa
lidyaT21 <156967948+lidyaT21@users.noreply.github.com>
2025-05-24 11:34:32 +0200

    Update based on reviews

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -5,15 +5,21 @@ const Footer = () => {
       <div className="section__container footer__container">
         <div className="footer__col">
           <h4>Quick Links</h4>
-          <ul className="footer__links"></ul>
+          <ul className="footer__links">
+            {/* TODO: Add quick links like Home, About, Services, etc. */}
+          </ul>
         </div>
         <div className="footer__col">
           <h4>Follow Us</h4>
-          <ul className="footer__links"></ul>
+          <ul className="footer__links">
+            {/* TODO: Add social media links like Facebook, Twitter, LinkedIn, etc. */}
+          </ul>
         </div>
         <div className="footer__col">
           <h4>Contact Us</h4>
-          <ul className="footer__links"></ul>
+          <ul className="footer__links">
+            {/* TODO: Add contact info like email, phone, or contact page link */}
+          </ul>
         </div>
       </div>
       <div className="footer__bar">
